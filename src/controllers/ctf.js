423bc4d5b206fe35bf17e67c8701c37c79512de7
mbahdashych <mbahdashych@cryptovoucher.io>
2021-12-31 14:49:40 +0100

    req fix

--- src/controllers/ctf.js
+++ src/controllers/ctf.js
@@ -3,5 +3,6 @@ const ctfService = require('../services/ctfService')
 exports.getCtfById = async (req, res) => {
   const { id } = req.params
   const data = await ctfService.getCtfById(id)
+  console.log(data)
   res.json(data)
 }
--- src/controllers/general.js
+++ src/controllers/general.js
@@ -1,10 +1,25 @@
 const articleService = require('./../services/articleService')
+const tipService = require('./../services/tipService')
+const writeUpService = require('./../services/writeUpService')
+const ctfService = require('./../services/ctfService')
 
 exports.getLatestReleases = async (req, res) => {
   const { q } = req.params
-  const result = await articleService.getArticles(q)
-  result.forEach(item => {
+  const articles = await articleService.getArticles(q)
+  const ctfs = await ctfService.getCtfs(q)
+  const writeups = await writeUpService.getWriteUps(q)
+  const data = []
+  articles.forEach(item => {
     item['type'] = 'article'
+    data.push(item)
   })
-  res.json(result)
+  ctfs.forEach(item => {
+    item['type'] = 'ctf'
+    data.push(item)
+  })
+  writeups.forEach(item => {
+    item['type'] = 'writeup'
+    data.push(item)
+  })
+  res.json(data)
 }
