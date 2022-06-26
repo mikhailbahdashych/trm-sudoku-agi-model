85ba2651f88e63f8f84101e627722901d52cff81
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-26 18:34:21 +0200

    jwt services

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,17 +1,43 @@
-const twoFactorService = require('node-2fa')
-const moment = require('moment')
-const dotenv = require('dotenv')
-const seedrandom = require('seedrandom')
-dotenv.config()
+const twoFactorService = require('node-2fa');
+const dotenv = require('dotenv');
+const seedrandom = require('seedrandom');
+dotenv.config();
 
 const userService = require('../services/userService');
 const cryptoService = require('../services/cryptoService');
+const jwtService = require('../services/jwtService');
+
 const loggerInstance = require('../common/logger');
-const logger = loggerInstance({ label: 'client-controller', path: 'client' })
+const logger = loggerInstance({ label: 'client-controller', path: 'client' });
 
 exports.signIn = async (req, res) => {
   try {
+    let { email, password, twofa } = req.body
 
+    if (!email || !password)
+      return res.status(400).json({ message: 'bad-request', status: 400 })
+
+    password = cryptoService.hashPassword(password, process.env.CRYPTO_SALT.toString())
+    const client = await userService.getClientToSignIn({email, password})
+    logger.info(`Sign in client with email: ${email}`)
+
+    if (!client) {
+      logger.info(`Wrong data while sign in for client with email: ${email}`)
+      return res.status(401).json({ message: 'unauthorized', status: 401 })
+    }
+
+    if (client.twofa) {
+      if (!twofa) return res.status(200).json({ twofa: true })
+
+      const result2Fa = twoFactorService.verifyToken(client.twofa, twofa)
+      if (!result2Fa) return res.status(403).json({ status: 403, message: 'access-forbidden' })
+      if (result2Fa.delta !== 0) return res.status(403).json({ status: 403, message: 'access-forbidden' })
+    }
+
+    const uxd = cryptoService.encrypt(client.id, process.env.CRYPTO_KEY.toString(), process.env.CRYPTO_IV.toString())
+    const token = jwtService.sign({ uxd });
+
+    return res.status(200).json(token)
   } catch (e) {
     logger.error(`Something went wrong while sign in => ${e}`)
     return res.status(500).json({ message: 'something-went-wrong', status: 500 })
@@ -22,9 +48,8 @@ exports.signUp = async (req, res) => {
   try {
     let { email, password } = req.body
 
-    if (!email || !password) {
+    if (!email || !password)
       return res.status(400).json({ message: 'bad-request', status: 400 })
-    }
 
     const user = await userService.getUserByEmail(email)
     logger.info(`Registration client with email: ${email}`)
--- src/services/userService.js
+++ src/services/userService.js
@@ -7,6 +7,12 @@ module.exports = {
       .where('email', email)
       .first()
   },
+  async getClientToSignIn(data) {
+    return knex(tableName)
+      .where('email', data.email)
+      .andWhere('password', data.password)
+      .first()
+  },
   async createUser(data) {
     return knex(tableName).insert(data)
   }
