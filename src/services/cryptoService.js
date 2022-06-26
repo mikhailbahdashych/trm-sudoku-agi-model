51de353bb6a4f3cea8555a373384bb39c1dfa519
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-26 19:06:37 +0200

    exports ifx

--- src/services/cryptoService.js
+++ src/services/cryptoService.js
@@ -1,33 +1,31 @@
 const crypto = require('crypto')
 
-export const encrypt = (text, key, iv) => {
-  let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
-  let encrypted = cipher.update(text, 'utf8', 'base64');
-  encrypted += cipher.final('base64');
-  return encrypted;
-}
-
-export const decrypt = (text, key, iv) => {
-  let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
-  let decrypted = decipher.update(text, 'base64', 'utf8');
-  return (decrypted + decipher.final('utf8'));
-}
-
-export const hashPassword = (password, key) => {
-  const sha256Hasher = crypto.createHmac("sha256", key);
-  return sha256Hasher.update(password).digest("hex");
-}
-
-export const encryptHex = (text, key, iv) => {
-  const cipher = crypto.createCipheriv('des-ecb', key, iv);
-  let encrypted = cipher.update(text, 'utf8', 'hex');
-  encrypted += cipher.final('hex');
-  return encrypted;
-}
-
-export const decryptHex = (text, key, iv) => {
-  const decipher = crypto.createDecipheriv('des-ecb', key, iv);
-  let decrypted = decipher.update(text, 'hex', 'utf8');
-  decrypted += decipher.final('utf8');
-  return decrypted
+module.exports = {
+  encrypt(text, key, iv) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', key, iv);
+    let encrypted = cipher.update(text, 'utf8', 'base64');
+    encrypted += cipher.final('base64');
+    return encrypted;
+  },
+  decrypt(text, key, iv) {
+    let decipher = crypto.createDecipheriv('aes-256-cbc', key, iv);
+    let decrypted = decipher.update(text, 'base64', 'utf8');
+    return (decrypted + decipher.final('utf8'));
+  },
+  hashPassword(password, key) {
+    const sha256Hasher = crypto.createHmac("sha256", key);
+    return sha256Hasher.update(password).digest("hex");
+  },
+  encryptHex(text, key, iv) {
+    const cipher = crypto.createCipheriv('des-ecb', key, iv);
+    let encrypted = cipher.update(text, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
+  },
+  decryptHex(text, key, iv) {
+    const decipher = crypto.createDecipheriv('des-ecb', key, iv);
+    let decrypted = decipher.update(text, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted
+  }
 }
--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -7,35 +7,35 @@ dotenv.config();
 const privateKey = fs.readFileSync(path.resolve(__dirname + "../../../keys/private.pem"));
 const publicKey = fs.readFileSync(path.resolve(__dirname + "../../../keys/public.pem"));
 
-export const sign = (payload) => {
-  return jwt.sign(
-    payload,
-    {
-      key: privateKey,
-      passphrase: process.env.JWT_PASSPHRASE.toString()
-    },
-    {
-      algorithm: "RS256",
-      expiresIn: "7d"
-    }
-  )
-}
-
-export const getClient = async (token) => {
-  return jwt.verify(
-    token,
-    publicKey
-  )
-}
-
-export const getClientPromise = (token) => {
-  return new Promise(((resolve, reject) => {
-    jwt.verify(token, publicKey, (err, decoded) => {
-      if (!err) {
-        return resolve(decoded);
-      } else {
-        return reject(err);
+module.exports = {
+  sign(payload) {
+    return jwt.sign(
+      payload,
+      {
+        key: privateKey,
+        passphrase: process.env.JWT_PASSPHRASE.toString()
+      },
+      {
+        algorithm: "RS256",
+        expiresIn: "7d"
       }
-    })
-  }))
+    )
+  },
+  async getClient(token) {
+    return jwt.verify(
+      token,
+      publicKey
+    )
+  },
+  getClientPromise(token) {
+    return new Promise(((resolve, reject) => {
+      jwt.verify(token, publicKey, (err, decoded) => {
+        if (!err) {
+          return resolve(decoded);
+        } else {
+          return reject(err);
+        }
+      })
+    }))
+  }
 }
