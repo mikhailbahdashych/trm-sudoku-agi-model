1c9e5920955b3133b6f45378329aced272ac60d0
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-26 20:41:53 +0200

    jwt export fix

--- src/services/jwtService.js
+++ src/services/jwtService.js
@@ -7,35 +7,32 @@ dotenv.config();
 const privateKey = fs.readFileSync(path.resolve(__dirname + "../../../keys/private.pem"));
 const publicKey = fs.readFileSync(path.resolve(__dirname + "../../../keys/public.pem"));
 
-module.exports = {
-  sign(payload) {
-    return jwt.sign(
-      payload,
-      {
-        key: privateKey,
-        passphrase: process.env.JWT_PASSPHRASE.toString()
-      },
-      {
-        algorithm: "RS256",
-        expiresIn: "7d"
+exports.sign = payload => {
+  return jwt.sign(
+    payload,
+    {
+      key: privateKey,
+      passphrase: process.env.JWT_PASSPHRASE.toString()
+    },
+    {
+      algorithm: "RS256",
+      expiresIn: "7d"
+    }
+  )
+}
+
+exports.getClient = token => {
+  return jwt.verify(token, publicKey)
+}
+
+exports.getClientPromise = token => {
+  return new Promise(((resolve, reject) => {
+    jwt.verify(token, publicKey, (err, decoded) => {
+      if (!err) {
+        return resolve(decoded);
+      } else {
+        return reject(err);
       }
-    )
-  },
-  async getClient(token) {
-    return jwt.verify(
-      token,
-      publicKey
-    )
-  },
-  getClientPromise(token) {
-    return new Promise(((resolve, reject) => {
-      jwt.verify(token, publicKey, (err, decoded) => {
-        if (!err) {
-          return resolve(decoded);
-        } else {
-          return reject(err);
-        }
-      })
-    }))
-  }
+    })
+  }))
 }
