2e264d5f4652693220ffa22e3a5f1a47ca76e31f
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-06-26 10:21:22 +0200

    remove useless controllers and services

--- src/controllers/email.js
+++
@@ -1,6 +0,0 @@
-const emailService = require('../services/emailService')
-
-exports.sendEmail = async (req, res) => {
-  const data = await emailService.sendEmail(req.body)
-  res.json(data)
-}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
-const wrapAsync = require('./../middlewares/async')
+const wrapAsync = require('./../middlewares/async');
 
-const generalController = require('../controllers/general')
-const emailController = require('../controllers/email')
 const userController = require('../controllers/userController');
 
 router.post(
@@ -13,36 +11,6 @@ router.post(
 router.post(
   "/sign-up",
   wrapAsync(userController.signUn)
-)
-
-router.get(
-  "/get-selected-releases/:q",
-  wrapAsync(generalController.getSelectedReleases)
-)
-
-router.get(
-  "/get-latest-releases/:q",
-  wrapAsync(generalController.getLatestReleases)
-)
-
-router.get(
-  "/get-post/:id",
-  wrapAsync(generalController.getPostById)
-)
-
-router.get(
-  "/get-posts-by-category/:category/:from/:to",
-  wrapAsync(generalController.getPostsByCategory)
-)
-
-router.post(
-  "/search",
-  wrapAsync(generalController.search)
-)
-
-router.post(
-  "/send-email",
-  wrapAsync(emailController.sendEmail)
-)
+);
 
 module.exports = router;
