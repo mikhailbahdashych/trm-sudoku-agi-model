d42fc5da4cb048e4e5067bc285ee54290ce35b3c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-07-10 11:15:56 +0200

    basic auth

--- src/middlewares/basicAuth.js
+++ src/middlewares/basicAuth.js
@@ -9,7 +9,7 @@ module.exports = (req, res, next) => {
   if (!auth)
     return res.status(401).json({ message: "unauthorized", status: 401 })
 
-  const credentials = new Buffer(auth.split(' ').pop(), 'base64').toString('ascii').split(':');
+  const credentials = new Buffer.from(auth.split(' ').pop(), 'base64').toString('ascii').split(':');
 
   if (credentials[0] === basicUsername && credentials[1] === basicPassword) next()
   else return res.status(401).json({ message: "unauthorized", status: 401 })
