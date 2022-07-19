150aa5c843dfc1295800aac2146e5bf74383f72c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-07-19 18:34:02 +0200

    middleware fix

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,12 +1,12 @@
 exports.v = fields => {
   return (req, res, next) => {
-    fields.forEach(field => {
+    for (const field of fields) {
       if (req.body[field]) {
         const result = require(`./validators/${field}`)(req.body[field])
         if (!result)
           return res.status(400).json({ message: 'bad-request', status: 400 })
       }
-    })
+    }
     next()
   }
 }
