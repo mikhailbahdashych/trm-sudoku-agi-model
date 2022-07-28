c356550681c986b7c8683887b1af08c7d61bcaaf
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-07-28 17:17:33 +0200

    restfulizing api

--- src/routes.js
+++ src/routes.js
@@ -14,45 +14,43 @@ const basicAuth = require('./middlewares/basicAuth');
 
 // @TODO Do research about white-space: pre-line and VueEditor
 
-router.post('/sign-in', basicAuth, v(['email', 'password', 'phone', 'twoFa']), wrapAsync(userController.signIn));
-router.post('/sign-up', basicAuth, v(['email', 'password', 'username', 'personalInformation']), wrapAsync(userController.signUp));
-router.post('/change-password', v(['password', 'newPassword', 'newPasswordRepeat', 'twoFa']), authMiddleware, wrapAsync(userController.changePassword));
-router.post('/change-email', v(['email', 'twoFa']), authMiddleware, wrapAsync(userController.changeEmail));
-router.post('/set-2fa', v(['twoFa']), authMiddleware, wrapAsync(userController.setTwoFa));
-router.post('/disable-2fa', v(['twoFa']), authMiddleware, wrapAsync(userController.disableTwoFa));
-router.post('/set-mobile-phone', v(['twoFa']), authMiddleware, wrapAsync(userController.setMobilePhone));
-router.post('/disable-mobile-phone', v(['twoFa']), authMiddleware, wrapAsync(userController.disableMobilePhone));
-router.post('/delete-account', v(['password', 'twoFa']), authMiddleware, wrapAsync(userController.deleteAccount));
+router.post('/user/sign-in', basicAuth, v(['email', 'password', 'phone', 'twoFa']), wrapAsync(userController.signIn));
+router.post('/user/sign-up', basicAuth, v(['email', 'password', 'username', 'personalInformation']), wrapAsync(userController.signUp));
+router.get('/user/:personalId', basicAuth, wrapAsync(userController.getUserByPersonalId));
+router.get('/user/settings/:t', authMiddleware, wrapAsync(userController.getUserSettings));
+router.patch('/user/password', v(['password', 'newPassword', 'newPasswordRepeat', 'twoFa']), authMiddleware, wrapAsync(userController.changePassword));
+router.patch('/user/email', v(['email', 'twoFa']), authMiddleware, wrapAsync(userController.changeEmail));
+router.patch('/user/2fa/set', v(['twoFa']), authMiddleware, wrapAsync(userController.setTwoFa));
+router.patch('/user/2fa/disable', v(['twoFa']), authMiddleware, wrapAsync(userController.disableTwoFa));
+router.patch('/user/mobile-phone/set', v(['twoFa']), authMiddleware, wrapAsync(userController.setMobilePhone));
+router.patch('/user/mobile-phone/disable', v(['twoFa']), authMiddleware, wrapAsync(userController.disableMobilePhone));
+router.patch('/user/delete-account', v(['password', 'twoFa']), authMiddleware, wrapAsync(userController.deleteAccount));
+router.patch('/user/personal-information', v(['personalInformation']), authMiddleware, wrapAsync(userController.updateUserPersonalInformation));
 
 router.post('/bookmark', authMiddleware, wrapAsync(userController.addBookmark));
-router.get('/get-bookmarks', authMiddleware, wrapAsync(userController.getBookmarks))
-router.delete('/delete-bookmark/:id', authMiddleware, wrapAsync(userController.deleteBookmark))
+router.get('/bookmark', authMiddleware, wrapAsync(userController.getBookmarks))
+router.delete('/bookmark/:id', authMiddleware, wrapAsync(userController.deleteBookmark))
 
-router.get('/get-refreshed-tokens', basicAuth, wrapAsync(userController.refreshToken));
-
-router.get('/get-user-by-personal-id/:personalId', basicAuth, wrapAsync(userController.getUserByPersonalId));
-router.get('/get-user-settings/:t', authMiddleware, wrapAsync(userController.getUserSettings));
+router.get('/refresh-tokens', basicAuth, wrapAsync(userController.refreshToken));
 
 router.get('/search', basicAuth, wrapAsync(searchController.search));
 
-router.patch('/update-user-personal-information', v(['personalInformation']), authMiddleware, wrapAsync(userController.updateUserPersonalInformation));
-
 router.patch('/vote/:id/:vote/:type', authMiddleware, wrapAsync(voteController.vote));
 
-router.get('/get-question', basicAuth, wrapAsync(questionController.getQuestion));
-router.get('/get-user-questions/:personalId/:sort', basicAuth, wrapAsync(questionController.getUserQuestions));
-router.get('/get-questions/:sort', basicAuth, wrapAsync(questionController.getQuestions));
-router.post('/create-question', v(['question']), authMiddleware, wrapAsync(questionController.createQuestion));
-router.post('/answer-question', authMiddleware, wrapAsync(questionController.answerQuestion));
+router.get('/question', basicAuth, wrapAsync(questionController.getQuestion));
+router.get('/question/:sort', basicAuth, wrapAsync(questionController.getQuestions));
+router.get('/user/:personalId/question/:sort', basicAuth, wrapAsync(questionController.getUserQuestions));
+router.post('/question', v(['question']), authMiddleware, wrapAsync(questionController.createQuestion));
+router.patch('/question/answer', authMiddleware, wrapAsync(questionController.answerQuestion));
 
-router.get('/get-blog-post', basicAuth, wrapAsync(blogController.getBlogPost));
-router.get('/get-blog-posts/:sort', basicAuth, wrapAsync(blogController.getBlogPosts));
-router.post('/create-blog-post', v(['post']), authMiddleware, wrapAsync(blogController.createBlogPost));
-router.post('/comment-blog-post', authMiddleware, wrapAsync(blogController.commentBlogPost));
+router.get('/blog-post', basicAuth, wrapAsync(blogController.getBlogPost));
+router.get('/blog-post/:sort', basicAuth, wrapAsync(blogController.getBlogPosts));
+router.post('/blog-post', v(['post']), authMiddleware, wrapAsync(blogController.createBlogPost));
+router.patch('/blog-post/comment', authMiddleware, wrapAsync(blogController.commentBlogPost));
 
-router.get('/get-forum-thread', basicAuth, wrapAsync(forumController.getForumThread));
-router.get('/get-forum-threads/:sort', basicAuth, wrapAsync(forumController.getForumThreads));
-router.post('/create-forum-thread', v(['thread']), authMiddleware, wrapAsync(forumController.createForumThread));
-router.post('/comment-forum-thread', authMiddleware, wrapAsync(forumController.commentForumThread))
+router.get('/forum-thread', basicAuth, wrapAsync(forumController.getForumThread));
+router.get('/forum-thread/:sort', basicAuth, wrapAsync(forumController.getForumThreads));
+router.post('/forum-thread', v(['thread']), authMiddleware, wrapAsync(forumController.createForumThread));
+router.patch('/forum-thread/comment', authMiddleware, wrapAsync(forumController.commentForumThread))
 
 module.exports = router;
